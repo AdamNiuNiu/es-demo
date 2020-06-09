d994e960b28fe039b4278082c7e63cdc63440cd1
adamLiu <306062998@qq.com>
2020-06-09 09:19:34 +0800

    0609更新swagger日期

--- bushu_demo/src/main/java/com/example/Swagger2.java
+++ bushu_demo/src/main/java/com/example/Swagger2.java
@@ -28,6 +28,6 @@ public class Swagger2 {
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder().title("Springboot测试api文档")
                 .description("").termsOfServiceUrl("")
-                .version("2020.06.07").build();
+                .version("2020.06.09").build();
     }
 }
