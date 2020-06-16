27809a7a5761aa423c012183c552dbfccce5cd87
adamLiu <306062998@qq.com>
2020-06-16 09:24:39 +0800

    es初始demo更改swagger日期，代码提交

--- esdemo/src/main/java/com/example/es/esdemo/Swagger2.java
+++ esdemo/src/main/java/com/example/es/esdemo/Swagger2.java
@@ -28,6 +28,6 @@ public class Swagger2 {
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder().title("Springboot-es-测试api文档")
                 .description("").termsOfServiceUrl("")
-                .version("2020.06.15").build();
+                .version("2020.06.16").build();
     }
 }
